b3ab5571bfa1317f9ef72c5a517aa44ca0d89e8e
Grayson Osborne <grayson.j.osborne.mil@army.mil>
2025-06-18 11:34:10 -0400

    working compose with frontend serving

--- backend/docker/entry.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-. /.venv/bin/activate
-
-  exec /.venv/bin/uvicorn main:app --port ${API_INFO_PORT:-8000} --host 0.0.0.0 --reload
-else
-  exec /.venv/bin/uvicorn main:app --port ${API_INFO_PORT:-8000} --host 0.0.0.0 --reload
-fi
